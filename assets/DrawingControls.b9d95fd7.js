206a58adc9dbe64153f619d1fa6bef27f535a09d
yjl9903 <yjl9903@users.noreply.github.com>
2021-10-20 11:14:07 +0000

    deploy: 7606d0318ce9b0d14efe1f9b1e787883343589e9

---
+++ assets/DrawingControls.b9d95fd7.js
@@ -0,0 +1,369 @@
+import { o as openBlock, c as createElementBlock, a as createBaseVNode, d as defineComponent, r as ref, u as useStorage, b as useDraggable, e as renderSlot, n as normalizeStyle, f as unref, g as createBlock, w as withCtx, h as normalizeClass, i as drawingEnabled, j as drawingPinned, k as createVNode, l as drawingMode, V as VerticalDivider, m as renderList, p as brushColors, F as Fragment, q as canUndo, s as canRedo, t as canClear, v as clearDrauu, x as withDirectives, y as vShow, z as createCommentVNode, A as drauu, _ as __unplugin_components_0, B as brush } from "./vendor.4cb4d544.js";
+const _hoisted_1$a = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$a = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M10 6v2h12.59L6 24.59L7.41 26L24 9.41V22h2V6H10z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$a = [
+  _hoisted_2$a
+];
+function render$9(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$a, _hoisted_3$a);
+}
+var __unplugin_components_1 = { name: "carbon-arrow-up-right", render: render$9 };
+const _hoisted_1$9 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$9 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 26a12 12 0 1 1 12-12a12 12 0 0 1-12 12z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$9 = [
+  _hoisted_2$9
+];
+function render$8(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$9, _hoisted_3$9);
+}
+var __unplugin_components_2 = { name: "carbon-radio-button", render: render$8 };
+const _hoisted_1$8 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$8 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M26 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM6 26V6h20v20z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$8 = [
+  _hoisted_2$8
+];
+function render$7(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$8, _hoisted_3$8);
+}
+var __unplugin_components_3 = { name: "carbon-checkbox", render: render$7 };
+const _hoisted_1$7 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$7 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M20 10H7.815l3.587-3.586L10 5l-6 6l6 6l1.402-1.415L7.818 12H20a6 6 0 0 1 0 12h-8v2h8a8 8 0 0 0 0-16z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$7 = [
+  _hoisted_2$7
+];
+function render$6(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$7, _hoisted_3$7);
+}
+var __unplugin_components_4 = { name: "carbon-undo", render: render$6 };
+const _hoisted_1$6 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$6 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M12 10h12.185l-3.587-3.586L22 5l6 6l-6 6l-1.402-1.415L24.182 12H12a6 6 0 0 0 0 12h8v2h-8a8 8 0 0 1 0-16z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$6 = [
+  _hoisted_2$6
+];
+function render$5(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$6, _hoisted_3$6);
+}
+var __unplugin_components_5 = { name: "carbon-redo", render: render$5 };
+const _hoisted_1$5 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$5 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M12 12h2v12h-2z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$5 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M18 12h2v12h-2z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_4$1 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_5 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M12 2h8v2h-8z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_6 = [
+  _hoisted_2$5,
+  _hoisted_3$5,
+  _hoisted_4$1,
+  _hoisted_5
+];
+function render$4(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$5, _hoisted_6);
+}
+var __unplugin_components_6 = { name: "carbon-delete", render: render$4 };
+const _hoisted_1$4 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$4 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M28.586 13.314L30 11.9L20 2l-1.314 1.415l1.186 1.186L8.38 14.322l-1.716-1.715L5.25 14l5.657 5.677L2 28.583L3.41 30l8.911-8.909L18 26.748l1.393-1.414l-1.716-1.716l9.724-11.49z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$4 = [
+  _hoisted_2$4
+];
+function render$3(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$4, _hoisted_3$4);
+}
+var __unplugin_components_7 = { name: "carbon-pin-filled", render: render$3 };
+const _hoisted_1$3 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$3 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M28.59 13.31L30 11.9L20 2l-1.31 1.42l1.18 1.18l-11.49 9.72l-1.72-1.71L5.25 14l5.66 5.68L2 28.58L3.41 30l8.91-8.91L18 26.75l1.39-1.42l-1.71-1.71l9.72-11.49zM16.26 22.2L9.8 15.74L21.29 6L26 10.71z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$3 = [
+  _hoisted_2$3
+];
+function render$2(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$3, _hoisted_3$3);
+}
+var __unplugin_components_8 = { name: "carbon-pin", render: render$2 };
+const _hoisted_1$2 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$2 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M2 16A14 14 0 1 0 16 2A14 14 0 0 0 2 16zm23.15 7.75L8.25 6.85a12 12 0 0 1 16.9 16.9zM8.24 25.16a12 12 0 0 1-1.4-16.89l16.89 16.89a12 12 0 0 1-15.49 0z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$2 = [
+  _hoisted_2$2
+];
+function render$1(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$2, _hoisted_3$2);
+}
+var __unplugin_components_9 = { name: "carbon-error", render: render$1 };
+const _hoisted_1$1 = {
+  class: "slidev-icon",
+  width: "1.2em",
+  height: "1.2em",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 32 32"
+};
+const _hoisted_2$1 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2zm0 26C9.4 28 4 22.6 4 16S9.4 4 16 4s12 5.4 12 12s-5.4 12-12 12z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3$1 = /* @__PURE__ */ createBaseVNode("path", {
+  d: "M21.4 23L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4z",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_4 = [
+  _hoisted_2$1,
+  _hoisted_3$1
+];
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$1, _hoisted_4);
+}
+var __unplugin_components_10 = { name: "carbon-close-outline", render };
+const _sfc_main$1 = /* @__PURE__ */ defineComponent({
+  props: {
+    storageKey: { type: String, required: false },
+    initial: { type: Object, required: false }
+  },
+  setup(__props) {
+    var _a;
+    const props = __props;
+    const el = ref(null);
+    const initial = (_a = props.initial) != null ? _a : { x: 0, y: 0 };
+    const point = props.storageKey ? useStorage(props.storageKey, initial) : ref(initial);
+    const { style } = useDraggable(el, { initialValue: point });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", {
+        ref: (_value, _refs) => {
+          _refs["el"] = _value;
+          el.value = _value;
+        },
+        class: "fixed",
+        style: normalizeStyle(unref(style))
+      }, [
+        renderSlot(_ctx.$slots, "default")
+      ], 4);
+    };
+  }
+});
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("svg", {
+  width: "1em",
+  height: "1em",
+  class: "-mt-0.5",
+  preserveAspectRatio: "xMidYMid meet",
+  viewBox: "0 0 24 24"
+}, [
+  /* @__PURE__ */ createBaseVNode("path", {
+    d: "M21.71 3.29a1 1 0 0 0-1.42 0l-18 18a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l18-18a1 1 0 0 0 0-1.42z",
+    fill: "currentColor"
+  })
+], -1);
+const _hoisted_2 = [
+  _hoisted_1
+];
+const _hoisted_3 = ["onClick"];
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  setup(__props) {
+    function undo() {
+      drauu.undo();
+    }
+    function redo() {
+      drauu.redo();
+    }
+    function setDrawingMode(mode) {
+      drawingMode.value = mode;
+      drawingEnabled.value = true;
+    }
+    function setBrushColor(color) {
+      brush.color = color;
+      drawingEnabled.value = true;
+    }
+    return (_ctx, _cache) => {
+      const _component_carbon58pen = __unplugin_components_0;
+      const _component_carbon58arrow_up_right = __unplugin_components_1;
+      const _component_carbon58radio_button = __unplugin_components_2;
+      const _component_carbon58checkbox = __unplugin_components_3;
+      const _component_carbon58undo = __unplugin_components_4;
+      const _component_carbon58redo = __unplugin_components_5;
+      const _component_carbon58delete = __unplugin_components_6;
+      const _component_carbon58pin_filled = __unplugin_components_7;
+      const _component_carbon58pin = __unplugin_components_8;
+      const _component_carbon58error = __unplugin_components_9;
+      const _component_carbon58close_outline = __unplugin_components_10;
+      return openBlock(), createBlock(_sfc_main$1, {
+        class: normalizeClass(["flex flex-wrap text-xl p-2 gap-1 rounded-md bg-main shadow transition-opacity duration-200 dark:border dark:border-gray-400 dark:border-opacity-10", unref(drawingEnabled) ? "" : unref(drawingPinned) ? "opacity-40 hover:opacity-90" : "opacity-0 pointer-events-none"]),
+        "storage-key": "slidev-drawing-pos",
+        "initial-x": 10,
+        "initial-y": 10
+      }, {
+        default: withCtx(() => [
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: unref(drawingMode) != "stylus" }]),
+            onClick: _cache[0] || (_cache[0] = ($event) => setDrawingMode("stylus"))
+          }, [
+            createVNode(_component_carbon58pen)
+          ], 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: unref(drawingMode) != "line" }]),
+            onClick: _cache[1] || (_cache[1] = ($event) => setDrawingMode("line"))
+          }, _hoisted_2, 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: unref(drawingMode) != "arrow" }]),
+            onClick: _cache[2] || (_cache[2] = ($event) => setDrawingMode("arrow"))
+          }, [
+            createVNode(_component_carbon58arrow_up_right)
+          ], 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: unref(drawingMode) != "ellipse" }]),
+            onClick: _cache[3] || (_cache[3] = ($event) => setDrawingMode("ellipse"))
+          }, [
+            createVNode(_component_carbon58radio_button)
+          ], 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: unref(drawingMode) != "rectangle" }]),
+            onClick: _cache[4] || (_cache[4] = ($event) => setDrawingMode("rectangle"))
+          }, [
+            createVNode(_component_carbon58checkbox)
+          ], 2),
+          createVNode(VerticalDivider),
+          (openBlock(true), createElementBlock(Fragment, null, renderList(unref(brushColors), (color) => {
+            return openBlock(), createElementBlock("button", {
+              key: color,
+              class: normalizeClass(["icon-btn", unref(brush).color === color ? "active" : "shallow"]),
+              onClick: ($event) => setBrushColor(color)
+            }, [
+              createBaseVNode("div", {
+                class: normalizeClass(["w-6 h-6 transition-all transform border border-gray-400/50", unref(brush).color !== color ? "rounded-1/2 scale-85" : "rounded-md"]),
+                style: normalizeStyle(unref(drawingEnabled) ? { background: color } : { borderColor: color })
+              }, null, 6)
+            ], 10, _hoisted_3);
+          }), 128)),
+          createVNode(VerticalDivider),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { disabled: !unref(canUndo) }]),
+            onClick: _cache[5] || (_cache[5] = ($event) => undo())
+          }, [
+            createVNode(_component_carbon58undo)
+          ], 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { disabled: !unref(canRedo) }]),
+            onClick: _cache[6] || (_cache[6] = ($event) => redo())
+          }, [
+            createVNode(_component_carbon58redo)
+          ], 2),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { disabled: !unref(canClear) }]),
+            onClick: _cache[7] || (_cache[7] = ($event) => unref(clearDrauu)())
+          }, [
+            createVNode(_component_carbon58delete)
+          ], 2),
+          createVNode(VerticalDivider),
+          createBaseVNode("button", {
+            class: normalizeClass(["icon-btn", { shallow: !unref(drawingPinned) }]),
+            onClick: _cache[8] || (_cache[8] = ($event) => drawingPinned.value = !unref(drawingPinned))
+          }, [
+            withDirectives(createVNode(_component_carbon58pin_filled, { class: "transform -rotate-45" }, null, 512), [
+              [vShow, unref(drawingPinned)]
+            ]),
+            withDirectives(createVNode(_component_carbon58pin, null, null, 512), [
+              [vShow, !unref(drawingPinned)]
+            ])
+          ], 2),
+          unref(drawingEnabled) ? (openBlock(), createElementBlock("button", {
+            key: 0,
+            class: normalizeClass(["icon-btn", { shallow: !unref(drawingEnabled) }]),
+            onClick: _cache[9] || (_cache[9] = ($event) => drawingEnabled.value = !unref(drawingEnabled))
+          }, [
+            withDirectives(createVNode(_component_carbon58error, null, null, 512), [
+              [vShow, unref(drawingPinned)]
+            ]),
+            withDirectives(createVNode(_component_carbon58close_outline, null, null, 512), [
+              [vShow, !unref(drawingPinned)]
+            ])
+          ], 2)) : createCommentVNode("v-if", true)
+        ]),
+        _: 1
+      }, 8, ["class"]);
+    };
+  }
+});
+export { _sfc_main as default };
---
+++ assets/DrawingLayer.5702d90b.js
@@ -0,0 +1,25 @@
+import { d as defineComponent, D as inject, E as injectionSlideScale, r as ref, G as onMounted, A as drauu, H as watch, I as loadCanvas, J as onBeforeUnmount, o as openBlock, c as createElementBlock } from "./vendor.4cb4d544.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  setup(__props) {
+    const scale = inject(injectionSlideScale);
+    const svg = ref();
+    onMounted(() => {
+      drauu.mount(svg.value, svg.value.parentElement);
+      watch(scale, (scale2) => drauu.options.coordinateScale = 1 / scale2, { immediate: true });
+      loadCanvas();
+    });
+    onBeforeUnmount(() => {
+      drauu.unmount();
+    });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("svg", {
+        ref: (_value, _refs) => {
+          _refs["svg"] = _value;
+          svg.value = _value;
+        },
+        class: "w-full h-full absolute top-0 pointer-events-none"
+      }, null, 512);
+    };
+  }
+});
+export { _sfc_main as default };
